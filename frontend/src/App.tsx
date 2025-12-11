76ffb35082d38aa152e706190983205b96e12a25
calebe <calebearcilio@gmail.com>
2025-12-11 15:05:25 -0300

    Criação de componentes básicos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
-import Dashboard from "./pages/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
 import AuthLayout from "./components/layouts/AuthLayout";
 import DefaultLayout from "./components/layouts/DefaultLayout";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Pacientes from "./pages/Pacientes";
+import Medicos from "./pages/Medicos";
+import Consultas from "./pages/Consultas";
+import Account from "./pages/Account";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -15,6 +20,12 @@ function App() {
         {/* Rotas com cabeçalho */}
         <Route element={<DefaultLayout />}>
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/pacientes" element={<Pacientes />} />
+          <Route path="/medicos" element={<Medicos />} />
+          <Route path="/consultas" element={<Consultas />} />
+          <Route path="/conta" element={<Account />} />
+          <Route path="/perfil" element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
